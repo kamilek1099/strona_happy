694c94efb7e6044e00976b5442e5dd177efa13d3
kamilek1099 <61554443+kamilek1099@users.noreply.github.com>
2026-06-07 20:57:28 +0000

    Deploying to gh-pages from @ kamilek1099/strona_happy@a83c31cef47c614fd592a67c058c2ec0d401b0dc 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1280315716" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1574312358" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
